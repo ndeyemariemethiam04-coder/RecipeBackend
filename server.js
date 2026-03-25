363fdb2634ac18bc0af55d6048d00f053a904da2
Your Name <youremail@example.com>
2026-03-25 15:37:12 -0700

    Increase upload limit to 50MB

--- server.js
+++ server.js
@@ -13,7 +13,8 @@ getDb().then(() => {
     console.error("Database initialization failed", err)
 })
 
-app.use(express.json())
+app.use(express.json({ limit: "50mb" }))
+app.use(express.urlencoded({ limit: "50mb", extended: true }))
 app.use(cors())
 
 app.use("/auth", require("./routes/user"))
